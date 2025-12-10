8d88d726380747db8843ea1ac5e06858b28080e8
shubhammahale-Lab <shubhammahalee@gmail.com>
2025-12-10 12:40:05 +0530

    ArrayFileModified

--- 04-conditionStatement.sh
+++ 04-conditionStatement.sh
@@ -1,6 +1,9 @@
-#! /bin/bash
-USERCKE= ${id -u}
-if [ $USERCKE -eq 0 ]; then
+#!/bin/bash
+
+USERCKE=$(id -u)
+
+if [ "$USERCKE" -eq 0 ]
+then
     echo "You are root user"
 else
     echo "You are not root user"
