60e2e3e699360a03e070c7d4df33cc856547ff39
shubhammahale-Lab <shubhammahalee@gmail.com>
2025-12-10 12:27:08 +0530

    arrayOperations

---
+++ 04-array.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+# Array assignment
+persons=("Alice" "Bob" "Charlie")
+
+echo "arrays is ${persons[1]}"
+
+# Print each element in the array
+for person in "${persons[@]}"; do
+    echo "Hello, $person!"          
