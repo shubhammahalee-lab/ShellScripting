62a8833d59ba87e0b6f6ea07639b37505611fdfa
shubhammahale-Lab <shubhammahalee@gmail.com>
2025-12-10 12:31:31 +0530

    ArrayFileModified

--- 04-array.sh
+++ 04-array.sh
@@ -6,5 +6,7 @@ persons=("Alice" "Bob" "Charlie")
 echo "arrays is ${persons[1]}"
 
 # Print each element in the array
-for person in "${persons[@]}"; do
-    echo "Hello, $person!"          
+for person in "${persons[@]}"; 
+do
+    echo "Hello, $person!"  
+done        
