4c9dae8a757fb350f299074fe0534ea4317d2c37
shubhammahale-Lab <shubhammahalee@gmail.com>
2025-12-13 20:39:50 +0530

    ArrayFileModified

--- delete-old-logs.sh
+++ delete-old-logs.sh
@@ -7,13 +7,14 @@ DATE=$(date +%F_%H-%M-%S)
 SCRIPT_NAME=$(basename "$0")
 LOGFILE=$LOGSDIR/$SCRIPT_NAME-$DATE.log
 
-# create log directory automatically
+# create log directories automatically
 mkdir -p "$LOGSDIR"
+mkdir -p "$APP_LOGS_DIR"
 
 echo "Script started at $DATE" &>>"$LOGFILE"
 
 # find and delete log files older than 14 days
-find "$APP_LOGS_DIR" -name "*.log" -type f -mtime +14 | while read -r file
+find "$APP_LOGS_DIR" -name "*.log" -type f -mtime +14 2>>"$LOGFILE" | while read -r file
 do
     echo "Deleting $file" &>>"$LOGFILE"
     rm -f "$file"
